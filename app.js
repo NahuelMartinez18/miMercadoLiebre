47d03ff77a28c6546544559b31e8115df22ef966
NahuelMartinez18 <rod.marboza@gmail.com>
2023-07-27 14:46:50 -0300

    Animaciones añadidas, footer y formularios terminados.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-//! Clases 17 - 18 - 19 - 20 - 21
+//! Clases 17 - 18 - 19 - 20 - 21 - 22
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -12,8 +14,13 @@ app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "views", "home.html"))
 );
 
-app.post("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "views", "home.html"))
+app.get("/home", (req, res) =>
+  res.redirect("/"))
+
+app.post("/", (req, res) => {
+  console.log(req.body)
+  res.redirect("/")
+}
 );
 
 app.get("/register", (req, res) =>
@@ -23,6 +30,3 @@ app.get("/register", (req, res) =>
 app.get("/login", (req, res) =>
   res.sendFile(path.join(__dirname, "views", "login.html"))
 );
-
-
-
